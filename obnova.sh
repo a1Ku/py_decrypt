f40ef16c5a1a78b47bcd322f04a4ef35eedcb362
a1Ku <100774475+a1Ku@users.noreply.github.com>
2022-12-07 23:27:46 +0300

    Update obnova.sh

--- obnova.sh
+++ obnova.sh
@@ -6,23 +6,25 @@ case "$1" in
 dpkg --get-selections "*" && dpkg -l ;;
 
 -2)
-apt update -y && apt upgrade -y && apt dist-upgrade -y && apt --purge autoremove -y ;;
+apt update -y && apt upgrade -y && apt dist-upgrade -y && apt --purge autoremove -y
 
--3)
 lsb_release -a
 cat /etc/debian_version ;;
 
--4)
+-3)
 echo "deb http://mirror.yandex.ru/debian bullseye main" > /etc/apt/sources.list
-echo "deb-src http://mirror.yandex.ru/debian bullseye main" >> /etc/apt/sources.list
+echo "deb-src http://mirror.yandex.ru/debian bullseye main\n" >> /etc/apt/sources.list
 
 echo "deb http://mirror.yandex.ru/debian bullseye-updates main" >> /etc/apt/sources.list
-echo "deb-src http://mirror.yandex.ru/debian bullseye-updates main" >> /etc/apt/sources.list
+echo "deb-src http://mirror.yandex.ru/debian bullseye-updates main\n" >> /etc/apt/sources.list
 
 echo "deb http://security.debian.org/ bullseye-security main" >> /etc/apt/sources.list
-echo "deb-src http://security.debian.org/ bullseye-security main" >> /etc/apt/sources.list ;;
+echo "deb-src http://security.debian.org/ bullseye-security main" >> /etc/apt/sources.list
 
--5)
 apt update -y && apt upgrade -y && apt dist-upgrade ;;
+
+-4)
+lsb_release -a
+cat /etc/debian_version ;;
 
 esac
